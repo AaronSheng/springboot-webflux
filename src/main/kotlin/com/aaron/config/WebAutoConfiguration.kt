23eb14da76c026742fb70b8a875bb75e1f9d388a
AaronSheng <shengmac@126.com>
2019-04-14 16:37:32 +0800

    删除无关引用

--- src/main/kotlin/com/aaron/config/WebAutoConfiguration.kt
+++ src/main/kotlin/com/aaron/config/WebAutoConfiguration.kt
@@ -1,23 +1,7 @@
 package com.aaron.config
 
-import org.springframework.boot.autoconfigure.AutoConfigureBefore
-import org.springframework.boot.autoconfigure.AutoConfigureOrder
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
-import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.core.Ordered
-
 /**
  * Created by Aaron Sheng on 2018/6/12.
  */
 class WebAutoConfiguration{
-    @Bean
-    @Profile("!prod")
-    fun jerseySwaggerConfig() = JerseySwaggerConfig()
-
-    @Bean
-    @Profile("prod")
-    fun jerseyConfig() = JerseyConfig()
 }
